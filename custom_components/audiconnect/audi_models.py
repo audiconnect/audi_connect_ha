186b204ab74bbe0166a12646d01f1e68fc8d8f30
t0bias-r <github@tobias-rodde.de>
2021-12-03 09:22:22 +0100

    Changed vehicleList query to retrieve more information like model + name

--- custom_components/audiconnect/audi_models.py
+++ custom_components/audiconnect/audi_models.py
@@ -125,10 +125,14 @@ class Vehicle:
     def parse(self, data):
         self.vin = data.get("vin")
         self.csid = data.get("csid")
-        self.model = data.get("model")
-        self.model_year = data.get("model_year")
-        self.model_family = data.get("model_family")
-        self.title = data.get("title")
+        if data.get("vehicle") is not None and data.get("vehicle").get("media") is not None:
+            self.model = data.get("vehicle").get("media").get("longName")
+        if data.get("vehicle") is not None and data.get("vehicle").get("core") is not None:
+            self.model_year = data.get("vehicle").get("core").get("modelYear")
+        if data.get("nickname") is not None and len(data.get("nickname")) > 0:
+            self.title = nickname
+        elif data.get("vehicle") is not None and data.get("vehicle").get("media") is not None:
+            self.title = data.get("vehicle").get("media").get("shortName")
 
     def __str__(self):
         return str(self.__dict__)
