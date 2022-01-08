c2ad6e1ae888ced6cf9462237e3a81439b011c02
t0bias-r <github@tobias-rodde.de>
2022-01-08 11:28:38 +0100

    New items + fixes: - Added: shortterm_current, shortterm_reset, longterm_current, longterm_reset - Added: outdoor_temperature - Added: charging_mode - Added: energy_flow - Fixed: X-Client-ID in __get_headers() - Fixed: parking position fix (error 204) - Fixed: calculation of actualChargeRate

--- custom_components/audiconnect/audi_models.py
+++ custom_components/audiconnect/audi_models.py
@@ -36,6 +36,45 @@ class VehicleDataResponse:
                 self.data_fields.append(Field(raw_field))
 
 
+class TripDataResponse:
+    def __init__(self, data):
+        self.data_fields = []
+
+        self.tripID = data["tripID"]
+
+        self.averageElectricEngineConsumption = None
+        if "averageElectricEngineConsumption" in data:
+             self.averageElectricEngineConsumption = float(data["averageElectricEngineConsumption"]) / 10
+
+        self.averageFuelConsumption = None
+        if "averageFuelConsumption" in data:
+            self.averageFuelConsumption = float(data["averageFuelConsumption"]) / 10
+
+        self.averageSpeed = None
+        if "averageSpeed" in data:
+            self.averageSpeed = int(data["averageSpeed"])
+
+        self.mileage = None
+        if "mileage" in data:
+            self.mileage = int(data["mileage"])
+
+        self.startMileage = None
+        if "startMileage" in data:
+            self.startMileage = int(data["startMileage"])
+
+        self.traveltime = None
+        if "traveltime" in data:
+            self.traveltime = int(data["traveltime"])
+
+        self.timestamp = None
+        if "timestamp" in data:
+            self.timestamp = data["timestamp"]
+
+        self.overallMileage = None
+        if "overallMileage" in data:
+            self.overallMileage = int(data["overallMileage"])
+
+
 class Field:
     IDS = {
         "0x0": "UNKNOWN",
