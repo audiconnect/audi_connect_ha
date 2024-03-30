c00799cebb04cdb03f9c05eaeba3ada68e68aa0f
coreywillwhat <104224685+coreywillwhat@users.noreply.github.com>
2024-03-29 18:21:42 -0600

    Update audi_account.py

--- custom_components/audiconnect/audi_account.py
+++ custom_components/audiconnect/audi_account.py
@@ -22,6 +22,13 @@ from .audi_models import VehicleData
 from .const import (
     DOMAIN,
     CONF_VIN,
+    CONF_CLIMATE_TEMP_F,
+    CONF_CLIMATE_TEMP_C,
+    CONF_CLIMATE_GLASS,
+    CONF_CLIMATE_SEAT_FL,
+    CONF_CLIMATE_SEAT_FR,
+    CONF_CLIMATE_SEAT_RL,
+    CONF_CLIMATE_SEAT_RR,
     CONF_ACTION,
     CONF_REGION,
     CONF_SPIN,
