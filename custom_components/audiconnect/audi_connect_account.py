1e7c972f0881fa3d54dc4e5f4454c1be38a78578
Kolbi <MatthiasKolb@2-wheel-planet.de>
2024-04-26 07:46:33 +0200

    fix: return SOC as INT instead of FLOAT (#411)
    Co-authored-by: coreywillwhat <104224685+coreywillwhat@users.noreply.github.com>

--- custom_components/audiconnect/audi_connect_account.py
+++ custom_components/audiconnect/audi_connect_account.py
@@ -1627,13 +1627,11 @@ class AudiConnectVehicle:
     def state_of_charge(self):
         """Return state of charge"""
         if self.state_of_charge_supported:
-            return parse_float(self._vehicle.state.get("stateOfCharge"))
+            return parse_int(self._vehicle.state.get("stateOfCharge"))
 
     @property
     def state_of_charge_supported(self):
-        check = self._vehicle.state.get("stateOfCharge")
-        if check and parse_float(check):
-            return True
+        return parse_int(self._vehicle.state.get("stateOfCharge")) is not None
 
     @property
     def remaining_charging_time(self):
