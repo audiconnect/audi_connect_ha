862d952ba83d313b6fce802a5349292890a9ea58
Kolbi <MatthiasKolb@2-wheel-planet.de>
2025-03-12 10:09:28 +0100

    fix: undo 471 to fix 495 (#527)

--- custom_components/audiconnect/audi_connect_account.py
+++ custom_components/audiconnect/audi_connect_account.py
@@ -1441,35 +1441,27 @@ class AudiConnectVehicle:
 
     @property
     def doors_trunk_status_supported(self):
-        checkLeftFront = self._vehicle.fields.get("OPEN_STATE_LEFT_FRONT_DOOR")
-        checkLeftRear = self._vehicle.fields.get("OPEN_STATE_LEFT_REAR_DOOR")
-        checkRightFront = self._vehicle.fields.get("OPEN_STATE_RIGHT_FRONT_DOOR")
-        checkRightRear = self._vehicle.fields.get("OPEN_STATE_RIGHT_REAR_DOOR")
-        checkTrunk = self._vehicle.fields.get("OPEN_STATE_TRUNK_LID")
-        if (
-            checkLeftFront
-            and checkLeftRear
-            and checkRightFront
-            and checkRightRear
-            and checkTrunk
-        ):
-            return True
+        return (
+            self.any_door_open_supported
+            and self.any_door_unlocked_supported
+            and self.trunk_open_supported
+            and self.trunk_unlocked_supported
+        )
 
     @property
     def doors_trunk_status(self):
-        if self.doors_trunk_status_supported:
-            checkLeftFront = self._vehicle.fields.get("OPEN_STATE_LEFT_FRONT_DOOR")
-            checkLeftRear = self._vehicle.fields.get("OPEN_STATE_LEFT_REAR_DOOR")
-            checkRightFront = self._vehicle.fields.get("OPEN_STATE_RIGHT_FRONT_DOOR")
-            checkRightRear = self._vehicle.fields.get("OPEN_STATE_RIGHT_REAR_DOOR")
-            checkTrunk = self._vehicle.fields.get("OPEN_STATE_TRUNK_LID")
-            return not (
-                checkLeftFront == "3"
-                and checkLeftRear == "3"
-                and checkRightFront == "3"
-                and checkRightRear == "3"
-                and checkTrunk == "3"
-            )
+        if (
+            self.any_door_open_supported
+            and self.any_door_unlocked_supported
+            and self.trunk_open_supported
+            and self.trunk_unlocked_supported
+        ):
+            if self.any_door_open or self.trunk_open:
+                return "Open"
+            elif self.any_door_unlocked or self.trunk_unlocked:
+                return "Closed"
+            else:
+                return "Locked"
 
     @property
     def trunk_unlocked(self):
