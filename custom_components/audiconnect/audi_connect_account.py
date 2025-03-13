297e51d7c886c1dc5c0194a92954b56e9b887528
Kolbi <MatthiasKolb@2-wheel-planet.de>
2025-03-13 11:10:43 +0100

    Update audi_connect_account.py

--- custom_components/audiconnect/audi_connect_account.py
+++ custom_components/audiconnect/audi_connect_account.py
@@ -1741,7 +1741,8 @@ class AudiConnectVehicle:
     def plug_state(self):
         """Return plug state"""
         if self.plug_state_supported:
-            return self._vehicle.state.get("plugState")
+            check = self._vehicle.state.get("plugState")
+            return check != "disconnected"
 
     @property
     def plug_state_supported(self):
