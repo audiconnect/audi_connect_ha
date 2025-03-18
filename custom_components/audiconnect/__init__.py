fc49d1d94c3232395f464b8176faa2845d9b2394
coreywillwhat <104224685+coreywillwhat@users.noreply.github.com>
2025-03-18 05:50:13 -0600

    Update __init__.py

--- custom_components/audiconnect/__init__.py
+++ custom_components/audiconnect/__init__.py
@@ -28,6 +28,8 @@ from .const import (
     MIN_UPDATE_INTERVAL,
     RESOURCES,
     COMPONENTS,
+    CONF_API_LEVEL,
+    API_LEVELS,
 )
 
 _LOGGER = logging.getLogger(__name__)
@@ -86,9 +88,18 @@ async def async_setup(hass, config):
     return True
 
 
+async def async_update_listener(hass, config_entry):
+    #_LOGGER.debug("The Update Listener has not been configured yet...")
+    raise NotImplementedError
+
+
 async def async_setup_entry(hass, config_entry):
     """Set up this integration using UI."""
     _LOGGER.debug("Audi Connect starting...")
+    
+    # Register the update listener so that changes to configuration options are applied immediately.
+    config_entry.async_on_unload(config_entry.add_update_listener(async_update_listener))
+    
     if DOMAIN not in hass.data:
         hass.data[DOMAIN] = {}
 
