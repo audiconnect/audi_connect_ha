856bf29612c3131b520d31dcf2587c2023284737
Pastaloverzzz <155114099+Pastaloverzzz@users.noreply.github.com>
2026-06-12 23:01:45 +0200

    Update device_tracker.py
    Fix deprecated importing TrackerEntity and SourceType

--- custom_components/audiconnect/device_tracker.py
+++ custom_components/audiconnect/device_tracker.py
@@ -4,8 +4,8 @@ from __future__ import annotations
 
 from typing import Any
 
-from homeassistant.components.device_tracker import SourceType
-from homeassistant.components.device_tracker.config_entry import TrackerEntity
+from homeassistant.components.device_tracker import TrackerEntity
+from homeassistant.components.device_tracker.const import SourceType
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
